309b5db73d3597d584727eb766eea0d119db95cd
Ajinkya <asbadve@gmail.com>
2016-07-26 15:10:56 +0530

    final push

--- app/src/main/java/com/ajinkyabadve/myreddit/ReddIApplication.java
+++ app/src/main/java/com/ajinkyabadve/myreddit/ReddIApplication.java
@@ -3,14 +3,32 @@ package com.ajinkyabadve.myreddit;
 import android.app.Application;
 import android.content.Context;
 
+import org.acra.ACRA;
+import org.acra.ReportField;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
 import rx.Scheduler;
 import rx.schedulers.Schedulers;
 
 /**
  * Created by Ajinkya on 25/07/2016.
  */
+@ReportsCrashes(mailTo = "asbadve@gmail.com", customReportContent = {
+        ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME,
+        ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL,
+        ReportField.CUSTOM_DATA, ReportField.STACK_TRACE, ReportField.LOGCAT},
+        mode = ReportingInteractionMode.DIALOG, resDialogText = R.string.crash_toast_text,
+        resDialogIcon = android.R.drawable.ic_dialog_info, resDialogTitle = R.string.crash_title)
+
 public class ReddIApplication extends Application {
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ACRA.init(this);
+
+    }
 
     private ReddItService openWeatherMapService;
     private Scheduler defaultSubscribeScheduler;
